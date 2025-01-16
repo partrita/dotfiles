acafbfd04db33fbbcc5fb91b21503aa3001055db
Taeyoon Kim <partrita@gmail.com>
2025-01-16 08:35:46 +0000

    init commit

--- README.md
+++ README.md
@@ -1,23 +1,16 @@
-# Eric's dotfiles
+# Taeyoon's dotfiles
 
 My dotfiles! :D I use these to customize my terminal wherever I go.
 
-# Wat?!
+# What?!
 
 Lots of `terminal` applications use a `.application_name_rc` as their configuration file. These are usually stored under the user's home directory, though sometimes stored elsewhere.
 
-I have placed my configurations here so that I can replication my terminal/computing environment in the computers that I use.
+I have placed my configurations here so that I can replicate my terminal/computing environment on the computers that I use.
 
 # Inspirations
 
-[Mathias' .dotfiles][1]
-
-# Installation (for myself)
-
-    $ cd ~
-    $ git clone git@github.com:ericmjl/dotfiles
-    $ cd dotfiles
-    $ bash install.sh
+[Mathias' .dotfiles](https://github.com/mathiasbynens/dotfiles)
 
 # Installation (for others)
 
@@ -25,14 +18,13 @@ Fork the repository before continuing.
 
     $ cd ~
 
-If you have SSH access enabled on Github:
+If you have SSH access enabled on GitHub:
 
     $ git clone git@github.com:[**username**]/dotfiles
 
 Otherwise, do:
 
     $ git clone https://github.com/[**username**]/dotfiles
+
     $ cd dotfiles
     $ bash install.sh
-
-[1]: https://github.com/mathiasbynens/dotfiles
--- install.linux.sh
+++ install.linux.sh
@@ -16,9 +16,9 @@ chmod +x ./install_scripts/install_homebrew_packages.sh
 chmod -x ./install_scripts/install_homebrew_packages.sh
 
 # Step 4: Install Anaconda
-chmod +x ./install_scripts/install_anaconda-linux.sh
-./install_scripts/install_anaconda-linux.sh
-chmod -x ./install_scripts/install_anaconda-linux.sh
+# chmod +x ./install_scripts/install_anaconda-linux.sh
+# ./install_scripts/install_anaconda-linux.sh
+# chmod -x ./install_scripts/install_anaconda-linux.sh
 
 # Step 5: Install Anthony Scopatz's nanorc files
 chmod +x ./install_scripts/install_nanorc.sh
@@ -26,21 +26,21 @@ chmod +x ./install_scripts/install_nanorc.sh
 chmod -x ./install_scripts/install_nanorc.sh
 
 # Step 6: Install oh-my-zsh
-chmod +x ./install_scripts/install_ohmyzsh.sh
-./install_scripts/install_ohmyzsh.sh
-chmod -x ./install_scripts/install_ohmyzsh.sh
+# chmod +x ./install_scripts/install_ohmyzsh.sh
+# ./install_scripts/install_ohmyzsh.sh
+# chmod -x ./install_scripts/install_ohmyzsh.sh
 
 # Step 7: Install Heroku
-chmod +x ./install_scripts/install_heroku-linux.sh
-./install_scripts/install_heroku-linux.sh
-chmod -x ./install_scripts/install_heroku-linux.sh
+# chmod +x ./install_scripts/install_heroku-linux.sh
+# ./install_scripts/install_heroku-linux.sh
+# chmod -x ./install_scripts/install_heroku-linux.sh
 
 # Step 8: Configure Git
 chmod +x ./install_scripts/configure_git.sh
 ./install_scripts/configure_git.sh
 chmod -x ./install_scripts/configure_git.sh
 
-# Step 8: Now install zshrc from this repo
+# Step 9: Now install zshrc from this repo
 chmod +x ./install_scripts/install_dotfiles.sh
 ./install_scripts/install_dotfiles.sh
 chmod -x ./install_scripts/install_dotfiles.sh
